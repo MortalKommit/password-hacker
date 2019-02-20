b9ccd7d21bae269fb76bad4a6327e534c3f49aaf
kedsko <kedsko@gmail.com>
2019-02-20 15:03:18 +0530

    Added recurse parameter switch, modularized Get-DirectoryFilesQueue

--- Get-DirectoryFiles/Get-DirectoryFilesQueue.psm1
+++ Get-DirectoryFiles/Get-DirectoryFilesQueue.psm1
@@ -1,30 +1,45 @@
 ﻿Function Get-DirectoryFilesQueue{
-param([Parameter(Mandatory = $true, Position = 0)][string]$path)
+param([Parameter(Mandatory = $true, Position = 0)][string]$path,
+[Parameter(Position = 1)][System.Management.Automation.SwitchParameter]$recurse)
 $testqueue = New-Object -Type System.Collections.Queue
 $rootpath = [System.IO.DirectoryInfo]$path
 $filecount = 0
 $testqueue.Enqueue($rootpath)
-while($testqueue.Count -gt 0){
-    $current = $testqueue.Dequeue()
+if(-not $recurse.IsPresent){
     try{
-        foreach($file in $current.EnumerateFiles()){
-           if(($file.Attributes -band [System.IO.FileAttributes]::System) -ne [System.IO.FileAttributes]::System){
+        foreach($file in ($testqueue.Dequeue()).EnumerateFiles()){
+            if(($file.Attributes -band [System.IO.FileAttributes]::System) -ne [System.IO.FileAttributes]::System){
                 $file
             }
-        }
+        }    
     }
     catch [System.UnauthorizedAccessException]{
         $error
     }
-    try{
-        foreach($dir in $current.EnumerateDirectories()){
-        if(($dir.Attributes -band [System.IO.FileAttributes]::System) -ne [System.IO.FileAttributes]::System){
-            $testqueue.Enqueue($dir)
+}
+else{
+    while($testqueue.Count -gt 0){
+        $current = $testqueue.Dequeue()
+        try{
+            foreach($file in $current.EnumerateFiles()){
+               if(($file.Attributes -band [System.IO.FileAttributes]::System) -ne [System.IO.FileAttributes]::System){
+                    $file
+                }
+            }
         }
+        catch [System.UnauthorizedAccessException]{
+            $error
         }
-    }
-    catch [System.UnauthorizedAccessException]{
-        $error
+        try{
+            foreach($dir in $current.EnumerateDirectories()){
+            if(($dir.Attributes -band [System.IO.FileAttributes]::System) -ne [System.IO.FileAttributes]::System){
+                $testqueue.Enqueue($dir)
+            }
+            }
+        }
+        catch [System.UnauthorizedAccessException]{
+            $error
+        }
     }
 }
 }
---
+++ Get-DirectoryFiles/Get-DirectoryFilesStack.psm1
@@ -0,0 +1,45 @@
+﻿Function Get-DirectoryFilesStack{
+param([Parameter(Mandatory = $true, Position = 0)][string]$path,
+[Parameter(Position = 1)][string][System.Management.Automation.SwitchParameter]$recurse)
+$pending = New-Object -Type System.Collections.Stack
+$rootpath = [System.IO.DirectoryInfo]$path
+$pending.Push($rootpath)
+if(-not $recurse.IsPresent){
+    $current = [System.IO.DirectoryInfo]$pending.Pop()
+    try{
+        foreach($file in $current.EnumerateFiles()){
+            if(($file.Attributes -band [System.IO.FileAttributes]::System) -ne [System.IO.FileAttributes]::System){
+                $file
+            }
+        }
+    }
+    catch [System.UnauthorizedAccessException]{
+        $error
+    }
+}
+else{
+    while($pending.Count -gt 0){
+        $current = [System.IO.DirectoryInfo]$pending.Pop()
+        foreach($dir in $current.EnumerateDirectories()){
+            try{
+                if(($dir.Attributes -band [System.IO.FileAttributes]::System) -ne [System.IO.FileAttributes]::System){
+                    $pending.Push($dir)
+                }
+            }
+            catch [System.UnauthorizedAccessException]{
+                 $error
+            }
+        }
+            foreach($file in $current.EnumerateFiles('*.*')){
+                try{
+                    if(($file.Attributes -band [System.IO.FileAttributes]::System) -ne [System.IO.FileAttributes]::System){
+                        $file
+                    }
+                }
+                catch [System.UnauthorizedAccessException]{
+                     $error
+                }
+            }
+    }
+}
+}
